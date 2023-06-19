ce6715fa7e7152cd7872ed194bff6d866612575e
sangeun <sangeun99@khu.ac.kr>
2023-06-19 11:14:28 +0900

    ♻️ refactor: 구구단 기능 함수로 구현

--- gugudan.py
+++ gugudan.py
@@ -1,7 +1,10 @@
 # 1: 구구단 출력하기
 
-for num in range(2, 10):
-    print("{}단".format(num))
-    for i in range (1, 10) :
-        print("{} x {} = {}".format(num, i, num*i))
-    print("\n")
+def gugudan() :
+    for num in range(2, 10):
+        print("{}단".format(num))
+        for i in range (1, 10) :
+            print("{} x {} = {}".format(num, i, num*i))
+        print("\n")
+
+gugudan()
