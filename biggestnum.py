ea891eae241eaa6a904d8bbf642398a1ac5e9f47
sangeun <sangeun99@khu.ac.kr>
2023-06-19 13:54:20 +0900

    ⚡️ feat: 최대 수 함수 추가

--- biggestnum.py
+++ biggestnum.py
@@ -1,9 +1,9 @@
-print("여러개의 숫자를 입력하시오 (공백으로 구분)")
-numlist = map(int, input().split())
+print("여러 개의 숫자를 입력하시오 (공백으로 구분)")
+numlist = input().split()
 
-max = numlist[0]
+max = int(numlist[0])
 for number in numlist :
-    if max < number:
-        max = number
+    if max < int(number):
+        max = int(number)
 
 print (max)
