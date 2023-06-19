e6c83ddf55134a33b7ad47e4e71b26d87fa54dc1
sangeun <sangeun99@khu.ac.kr>
2023-06-19 15:08:01 +0900

    ⚡️ feat: count을 이용해 마지막 인자 값에 도달했을 때만 append하도록 함

--- user.py
+++ user.py
@@ -4,16 +4,22 @@ users = [
     {"name": "Charlie", "age": 35, "location": "Daegu", "car": "Audi"},
 ]
 
-
 search_user = {"name": "Bob", "age": 30}
 
 def find_users(search_user) :
     result = []
+    count = 0
     for user in users :
         for key in search_user.keys() :
             if user[key] != search_user[key] :
                 break
-            result.append(user)
+            else :
+                count += 1
+                if count == len(search_user) :
+                    result.append(user)
     return result
             
 print(find_users(search_user))
+print(find_users({"name": "Bob"}))
+print(find_users({"name": "Bob", "location": "Busan", "car": "Mercedes"}))
+print(find_users({"location": "Busan", "car": "Mercedes"}))
