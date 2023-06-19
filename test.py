4b8b2d4494c8c5bcee75757adfb715383cb6ae33
sangeun <sangeun99@khu.ac.kr>
2023-06-19 11:07:23 +0900

    ✨ feat: 구구단 출력하기

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-print("hello")
+# 1: 구구단 출력하기
 
+for num in range(2, 10):
+    print("{}단".format(num))
+    for i in range (1, 10) :
+        print("{} x {} = {}".format(num, i, num*i))
+    print("\n")
 
-
-if (1) :
-    print("hi")
