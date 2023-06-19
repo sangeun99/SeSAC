95b381337cf43a9a4670c86f41658f1ba086e3d3
sangeun <sangeun99@khu.ac.kr>
2023-06-19 11:33:02 +0900

    ⚡️ feat: 파이썬 리스트 연습

--- test.py
+++ test.py
@@ -3,6 +3,12 @@ def get_name_and_age() :
     return "John", 25
 
 name, age = get_name_and_age()
+print(name, age)
 
-if __name__ == "__main__" :
-    print(name, age)
+# 리스트 - 배열 [] 형태로 관리
+shopping_list=["apple", "banana", "orange"]
+print(shopping_list)
+shopping_list.append("grape")
+print(shopping_list)
+shopping_list.remove("banana")
+print(shopping_list)
