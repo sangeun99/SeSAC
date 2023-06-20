660173979ee38baea3f00c92f5c109caa2c76c2e
sangeun <sangeun99@khu.ac.kr>
2023-06-20 18:09:51 +0900

    ⚡️ feat: mycalendar 초안 작성

--- findmax.py
+++ findmax.py
@@ -1,10 +1,19 @@
 def find_max(numlist):
-    max = int(numlist[0])
+    if len(numlist) == 0 :
+        return -1
+    max = numlist[0]
     for number in numlist :
-        if max < int(number):
-            max = int(number)
+        if max < number:
+            max = number
     return max
 
-print("여러 개의 숫자를 입력하시오 (공백으로 구분)")
-numlist = input().split()
+
+data1 = [1, 55, 123, 5]
+print("data1 최댓값 :", find_max(data1))
+
+numlist = input("여러 개의 숫자를 입력하시오: ").split()
 print ("최댓값 :", find_max(numlist))
+
+numlist2 = list(map(int, input().split()))
+text = input()
+numlist3 = [int(text) for text in text.split()]
