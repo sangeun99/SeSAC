5eb81d0c09c5f42e54ac9886860b1c8aa1765967
sangeun <sangeun99@khu.ac.kr>
2023-06-20 18:10:48 +0900

    Revert "⚡️ feat: mycalendar 초안 작성"
    This reverts commit 660173979ee38baea3f00c92f5c109caa2c76c2e.

--- findmax.py
+++ findmax.py
@@ -1,19 +1,10 @@
 def find_max(numlist):
-    if len(numlist) == 0 :
-        return -1
-    max = numlist[0]
+    max = int(numlist[0])
     for number in numlist :
-        if max < number:
-            max = number
+        if max < int(number):
+            max = int(number)
     return max
 
-
-data1 = [1, 55, 123, 5]
-print("data1 최댓값 :", find_max(data1))
-
-numlist = input("여러 개의 숫자를 입력하시오: ").split()
+print("여러 개의 숫자를 입력하시오 (공백으로 구분)")
+numlist = input().split()
 print ("최댓값 :", find_max(numlist))
-
-numlist2 = list(map(int, input().split()))
-text = input()
-numlist3 = [int(text) for text in text.split()]
