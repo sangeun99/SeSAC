a215c8b9d55dc46cad8f65984a791e98e50d6f50
sangeun <sangeun99@khu.ac.kr>
2023-06-29 15:41:04 +0900

    ⚡️ style: user탭 pagination 구현

--- 3.Flask/crm-page/app.py
+++ 3.Flask/crm-page/app.py
@@ -5,24 +5,29 @@ app = Flask(__name__, static_folder="static")
 
 @app.route("/")
 def root() :
-    return render_template("index.html")
+    return "hello"
 
 @app.route("/users")
-def user():
-    users = []
-    with open('src/data_space.csv', newline='', encoding="utf-8") as user:
+@app.route("/users/<int:index>")
+def user(index = 1):
+    per_page = 20
+    data = []
+    with open('src/user.csv', newline='', encoding="utf-8") as user:
         reader = csv.DictReader(user)
         next(reader)
         for row in reader:
             clean_row = {key.strip(): value.strip() for key, value in row.items()}
-            users.append(clean_row)
-    return render_template("users.html", users=users)
-
+            data.append(clean_row)
+    total_pages = int(len(data) / per_page) + 1
+    start_index = (index - 1) * per_page
+    end_index = index * per_page
+    users = data[start_index: end_index]
+    return render_template("users.html", users=users, total_pages=total_pages, index=index)
 
 @app.route("/user_detail/<userid>")
 def user_detail(userid):
     users = []
-    with open('src/data_space.csv', newline='', encoding="utf-8") as user:
+    with open('src/user.csv', newline='', encoding="utf-8") as user:
         reader = csv.DictReader(user)
         next(reader)
         for row in reader:
@@ -30,7 +35,6 @@ def user_detail(userid):
             if clean_row['Id'] == userid :
                 userinfo = clean_row
     return render_template("user_detail.html", userinfo=userinfo)
-
 
 @app.route("/stores")
 def store():
@@ -53,7 +57,6 @@ def store_detail(storeid):
             if clean_row['Id'] == storeid :
                 storeinfo = clean_row
     return render_template("store_detail.html", storeinfo=storeinfo)
-
 
 if __name__=="__main__":
     app.run(port=8080, debug=True)
