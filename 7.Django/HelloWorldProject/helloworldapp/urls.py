bea01dc4842242a0b7a313137e071254582146f7
sangeun <sangeun99@khu.ac.kr>
2023-08-01 11:27:26 +0900

    ⚡️ feat: django 로그인 페이지 구성

--- 7.Django/HelloWorldProject/helloworldapp/urls.py
+++ 7.Django/HelloWorldProject/helloworldapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.home_view, name='home'),
     path('', views.hello_world, name='hello_world'),
 ]
--- 7.Django/HelloWorldProject/helloworldapp/views.py
+++ 7.Django/HelloWorldProject/helloworldapp/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def hello_world(request):
     return HttpResponse("Hello, World!")
+
+@login_required
+def home_view(request):
+    user = request.user
+    context = {
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email
+    }
+    return render(request, 'helloworldapp/home.html', context=context)
+
+def login_view(request):
+    if request.method == 'POST' :
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            print("로그인에 성공하였습니다")
+            login(request, user)
+            return redirect('home')
+        else : 
+            
+            return "로그인에 실패하였습니다"
+    return render(request, 'helloworldapp/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
