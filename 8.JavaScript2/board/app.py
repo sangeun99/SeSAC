3fb6f5be934a87ea9a9b7d06e431158ca8f6ab51
tori <sangeun99@khu.ac.kr>
2023-08-04 15:26:51 +0900

    🎉 init: ajax 이용한 게시판 페이지 만들기

---
+++ 8.JavaScript2/board/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, jsonify, redirect
+from database import Database
+
+app = Flask(__name__)
+db = Database()
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/create', methods=['post'])
+def create():
+    title = request.form['title']
+    message = request.form['message']
+
+    sql = "INSERT INTO board(title, message) VALUES (?, ?)"
+    db.execute(sql, (title, message))
+    db.commit()
+    return "ok"
+
+@app.route('/delete', methods=['post'])
+def delete():
+    id = request.form['id']
+    print(id)
+    sql = "DELETE FROM board WHERE id=?"
+    db.execute(sql, (id, ))
+    db.commit()
+    return "ok"
+
+@app.route('/update', methods=['post'])
+def update():
+    id = int(request.form['id'])
+    title = request.form['title']
+    message = request.form['message']
+
+    sql = "UPDATE board SET title=?, message=? WHERE id=?"
+    db.execute(sql, (title, message, id))
+    db.commit()
+    return "ok"
+
+@app.route('/list', methods=['get'])
+def list():
+    sql = "SELECT * FROM board"
+    result = db.execute_fetch(sql)
+
+    # list to dict
+    # example : {2 : {'title': title, 'message" : message}}
+    tuple_keys = ('id', 'title', 'message')
+    dict_list = []
+    for r in result:
+        dict_value = dict(zip(tuple_keys, r))
+        dict_list.append(dict_value)
+    return jsonify(dict_list)
+
+if __name__ == "__main__" :
+    app.run(debug=True)
